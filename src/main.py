d6a04ac23439e6fe952ea52c2df2525f609da8ab
John Paul Antonovich <AntonovichJP@gmail.com>
2019-04-30 16:40:51 -0700

    Added stuff

---
+++ src/main.py
@@ -0,0 +1,56 @@
+from random import randint
+from os import system, name 
+
+
+openMap = open('map.txt', 'r')
+map = list(openMap.read())
+openMap.close()
+
+# Key:
+# _ : wall
+# | : wall
+# - : interactable
+# @ : player
+# ! : NPC
+# # : enemy
+# empty space : nothing
+# % : stairs (leads the player up or down a level)
+
+def clear(): 
+  
+    if name == 'nt': 
+        system('cls') 
+    else: 
+        system('clear') 
+
+
+
+def loop():
+    global map
+    
+    for i in map:
+        print(i, end="")
+    userInput = input('\nWhat are you going to do? ')
+    beforeInput = map.index('@')
+    # 386
+    
+    if userInput.lower() == 'd':
+        if map[beforeInput + 1] == '_' or map[beforeInput+1] == '|':
+            input('You can\'t go that way! (Press any button to continue) ')
+        else:    
+            map[beforeInput + 1] = '@'
+            map[beforeInput] = ' '
+    elif userInput.lower() == 'a':
+        if map[beforeInput-1] == '_' or map[beforeInput-1] == '|':
+            input('You can\'t go that way! (Press any button to continue) ')
+        else:    
+            map[beforeInput-1] = '@'
+            map[beforeInput] = ' '
+    
+    clear()
+    
+    
+
+
+while True:
+    loop()
